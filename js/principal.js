97b33470ec4edda951098881d91148b6e86574f8
LuisDeLeon <luis.de.leeon@gmail.com>
2024-09-05 08:29:24 -0600

    [CHANGE]: Se hace responsivo los divs de regalo y contenido principal

--- js/principal.js
+++ js/principal.js
@@ -35,20 +35,25 @@ $(document).ready(function () {
                 const heightSobre = tamanioSobre.offsetHeight;
                 let tamanioContenidoPrincipal = 0;
                 let posicionAlturaContenidoPrincipal = 0;
-
+                let posicionAlturaSello = 0;
                 $("#alto").text(alto);
                 if (alto >= 768 && alto <= 1024) {
-                    tamanioContenidoPrincipal = heightContenidoPrincipal - 115;
+                    posicionAlturaContenidoPrincipal = ajusteAlturaContenidoTablets(alto, heightSobre);
+                    tamanioContenidoPrincipal = ajusteAlturaMesaRegaloTablets(alto, heightContenidoPrincipal);
+                    posicionAlturaSello = ajusteAlturaLogoTablets(alto, heightSobre);
+                    $(".circular-button").attr("style", "top: " + 60 + "% !important; width: " + 120 + "px !important;height: " + 120 + "px !important;");
+                    $(".logo-boton").attr("style", "top: " + posicionAlturaSello + "% !important;width: " + 100 + "px !important;height: " + 100 + "px !important;");
+
                 }
                 else if (alto < 768) {
-                    
-                    // tamanioContenidoPrincipal = heightContenidoPrincipal + 50;
                     tamanioContenidoPrincipal = ajusteAlturaMesaRegalo(alto, heightContenidoPrincipal);
-                    // posicionAlturaContenidoPrincipal = heightSobre - 175;
-                    posicionAlturaContenidoPrincipal = ajusteAlturaContenido(alto,heightSobre);
+                    posicionAlturaContenidoPrincipal = ajusteAlturaContenido(alto, heightSobre);
+                    posicionAlturaSello = ajusteAlturaLogo(alto, heightSobre);
+                    $(".logo-boton").attr("style", "top: " + posicionAlturaSello + "% !important;");
                 }
                 $("#overlayDiv").css({ top: posicionAlturaContenidoPrincipal + 'px' });
                 $(".new-div").css({ top: tamanioContenidoPrincipal + 'px' });
+                // $(".logo-boton").css({ top: posicionAlturaSello + '% !important' });
 
                 observer.disconnect();
             }
@@ -92,141 +97,151 @@ $(document).ready(function () {
     }
 
 
-    // function ajusteAlturaMesaRegalo(altura, tamanioDiv){
-    //     valor = 0;
-    //     switch(true){
-    //         case altura >300 && altura < 326:
-    //         valor = tamanioDiv - 20;
-    //         break;
-    //         case altura >325 && altura < 351:
-    //         valor = tamanioDiv - 5;
-    //         break;
-    //         case altura >350 && altura < 376:
-    //         valor = tamanioDiv + 10;
-    //         break;
-    //         case altura >375 && altura < 401:
-    //         valor = tamanioDiv + 25;
-    //         break;
-    //     }
-    //     return valor;
-    // }
-    // function ajusteAlturaMesaRegalo(altura, tamanioDiv) {
-    //     if (altura > 300 && altura < 326) {
-    //       return tamanioDiv - 20;
-    //     } else if (altura > 325 && altura < 351) {
-    //       return tamanioDiv - 5;
-    //     } else if (altura > 350 && altura < 376) {
-    //       return tamanioDiv + 10;
-    //     } else if (altura > 375 && altura < 401) {
-    //       return tamanioDiv + 25;
-    //     } else if (altura > 400 && altura < 426) {
-    //       return tamanioDiv + 40;
-    //     } else if (altura > 425 && altura < 451) {
-    //       return tamanioDiv + 55;
-    //     } else if (altura > 450 && altura < 476) {
-    //       return tamanioDiv + 70;
-    //     } else if (altura > 475 && altura < 501) {
-    //       return tamanioDiv + 85;
-    //     } else if (altura > 500 && altura < 526) {
-    //       return tamanioDiv + 100;
-    //     } else if (altura > 525 && altura < 551) {
-    //       return tamanioDiv + 115;
-    //     } else if (altura > 550 && altura < 576) {
-    //       return tamanioDiv + 130;
-    //     } else if (altura > 575 && altura < 601) {
-    //       return tamanioDiv + 145;
-    //     } else if (altura > 600 && altura < 626) {
-    //       return tamanioDiv + 160;
-    //     } else if (altura > 625 && altura < 651) {
-    //       return tamanioDiv + 175;
-    //     } else if (altura > 650 && altura < 676) {
-    //       return tamanioDiv + 190;
-    //     } else if (altura > 675 && altura < 701) {
-    //       return tamanioDiv + 205;
-    //     } else if (altura > 700 && altura < 726) {
-    //       return tamanioDiv + 220;
-    //     } else if (altura > 725 && altura < 751) {
-    //       return tamanioDiv + 235;
-    //     } else if (altura > 750 && altura < 776) {
-    //       return tamanioDiv + 250;
-    //     } else if (altura >= 775) {
-    //       return tamanioDiv + 265;
-    //     }
-    //     return 0; // default value if none of the conditions match
-    //   }
-
     function ajusteAlturaMesaRegalo(altura, tamanioDiv) {
         // Definir los rangos y ajustes en un array de objetos
         const rangos = [
-          { min: 300, max: 325, ajuste: 10},
-          { min: 326, max: 350, ajuste: 25 },
-          { min: 351, max: 375, ajuste: 35 },
-          { min: 376, max: 400, ajuste: 55 },
-          { min: 401, max: 425, ajuste: 65 },
-          { min: 426, max: 450, ajuste: 70 },
-          { min: 451, max: 475, ajuste: 80 },
-          { min: 476, max: 500, ajuste: 85 },
-          { min: 501, max: 525, ajuste: 90 },
-          { min: 526, max: 550, ajuste: 100 },
-          { min: 551, max: 575, ajuste: 110 },
-          { min: 576, max: 600, ajuste: 115 },
-          { min: 601, max: 625, ajuste: 125 },
-          { min: 626, max: 650, ajuste: 135 },
-          { min: 651, max: 675, ajuste: 190 },
-          { min: 676, max: 700, ajuste: 205 },
-          { min: 701, max: 725, ajuste: 220 },
-          { min: 726, max: 750, ajuste: 235 },
-          { min: 751, max: 775, ajuste: 250 },
-          { min: 776, max: Infinity, ajuste: 265 } // Para alturas mayores o iguales a 775
+            { min: 300, max: 325, ajuste: 10 },
+            { min: 326, max: 350, ajuste: 25 },
+            { min: 351, max: 375, ajuste: 35 },
+            { min: 376, max: 400, ajuste: 55 },
+            { min: 401, max: 425, ajuste: 65 },
+            { min: 426, max: 450, ajuste: 70 },
+            { min: 451, max: 475, ajuste: 80 },
+            { min: 476, max: 500, ajuste: 85 },
+            { min: 501, max: 525, ajuste: 90 },
+            { min: 526, max: 550, ajuste: 100 },
+            { min: 551, max: 575, ajuste: 110 },
+            { min: 576, max: 600, ajuste: 115 },
+            { min: 601, max: 625, ajuste: 125 },
+            { min: 626, max: 650, ajuste: 145 },
+            { min: 651, max: 675, ajuste: 150 },
+            { min: 676, max: 700, ajuste: 160 },
+            { min: 701, max: 725, ajuste: 180 },
+            { min: 726, max: 750, ajuste: 190 },
+            { min: 751, max: 775, ajuste: 190 },
+            { min: 776, max: Infinity, ajuste: 265 } // Para alturas mayores o iguales a 775
         ];
-      
+
         // Buscar el ajuste correspondiente para la altura dada
         for (const rango of rangos) {
-          if (altura >= rango.min && altura <= rango.max) {
-            return tamanioDiv + rango.ajuste;
-          }
+            if (altura >= rango.min && altura <= rango.max) {
+                return tamanioDiv + rango.ajuste;
+            }
         }
-      
+
         // Si no se encuentra ningún rango, retornar 0 (opcional)
         return 0;
-      }
-      
+    }
+
     function ajusteAlturaContenido(altura, tamanioDiv) {
         // Definir los rangos y ajustes en un array de objetos
         const rangos = [
-          { min: 300, max: 325, ajuste: -135  },
-          { min: 326, max: 350, ajuste: -135 },
-          { min: 351, max: 375, ajuste: -145 },
-          { min: 376, max: 400, ajuste: -145 },
-          { min: 401, max: 425, ajuste: -155 },
-          { min: 426, max: 450, ajuste: -160 },
-          { min: 451, max: 475, ajuste: -170 },
-          { min: 476, max: 500, ajuste: -190 },
-          { min: 501, max: 525, ajuste: -185 },
-          { min: 526, max: 550, ajuste: -190 },
-          { min: 551, max: 575, ajuste: -195 },
-          { min: 576, max: 600, ajuste: -200 },
-          { min: 601, max: 625, ajuste: -200 },
-          { min: 626, max: 650, ajuste: -200 },
-          { min: 651, max: 675, ajuste: 190 },
-          { min: 676, max: 700, ajuste: 205 },
-          { min: 701, max: 725, ajuste: 220 },
-          { min: 726, max: 750, ajuste: 235 },
-          { min: 751, max: 775, ajuste: 250 },
-          { min: 776, max: Infinity, ajuste: 265 } // Para alturas mayores o iguales a 775
+            { min: 300, max: 325, ajuste: -135 },
+            { min: 326, max: 350, ajuste: -135 },
+            { min: 351, max: 375, ajuste: -145 },
+            { min: 376, max: 400, ajuste: -145 },
+            { min: 401, max: 425, ajuste: -155 },
+            { min: 426, max: 450, ajuste: -160 },
+            { min: 451, max: 475, ajuste: -170 },
+            { min: 476, max: 500, ajuste: -190 },
+            { min: 501, max: 525, ajuste: -185 },
+            { min: 526, max: 550, ajuste: -190 },
+            { min: 551, max: 575, ajuste: -195 },
+            { min: 576, max: 600, ajuste: -200 },
+            { min: 601, max: 625, ajuste: -200 },
+            { min: 626, max: 650, ajuste: -200 },
+            { min: 651, max: 675, ajuste: -205 },
+            { min: 676, max: 700, ajuste: -210 },
+            { min: 701, max: 725, ajuste: -220 },
+            { min: 726, max: 750, ajuste: -230 },
+            { min: 751, max: 775, ajuste: -240 },
+            { min: 776, max: Infinity, ajuste: -250 } // Para alturas mayores o iguales a 775
         ];
-      
+
         // Buscar el ajuste correspondiente para la altura dada
         for (const rango of rangos) {
-          if (altura >= rango.min && altura <= rango.max) {
-            return tamanioDiv + rango.ajuste;
-          }
+            if (altura >= rango.min && altura <= rango.max) {
+                return tamanioDiv + rango.ajuste;
+            }
         }
-      
+
         // Si no se encuentra ningún rango, retornar 0 (opcional)
         return 0;
-      }
-      
-      
+    }
+    function ajusteAlturaContenidoTablets(altura, tamanioDiv) {
+        // Definir los rangos y ajustes en un array de objetos
+        const rangos = [
+            { min: 768, max: 800, ajuste: -150 },
+            { min: 801, max: 850, ajuste: -165 },
+            { min: 851, max: 900, ajuste: -180 },
+            { min: 901, max: 1024, ajuste: -185 }
+        ];
+
+        // Buscar el ajuste correspondiente para la altura dada
+        for (const rango of rangos) {
+            if (altura >= rango.min && altura <= rango.max) {
+                return tamanioDiv + rango.ajuste;
+            }
+        }
+
+        // Si no se encuentra ningún rango, retornar 0 (opcional)
+        return 0;
+    }
+
+    function ajusteAlturaMesaRegaloTablets(altura, tamanioDiv) {
+        // Definir los rangos y ajustes en un array de objetos
+        const rangos = [
+            { min: 768, max: 820, ajuste: 150 },
+            { min: 821, max: 875, ajuste: 165 },
+            { min: 876, max: 900, ajuste: 175 },
+            { min: 901, max: 950, ajuste: 200 }, // Para alturas mayores o iguales a 775
+            { min: 951, max: 1000, ajuste: 220 }, // Para alturas mayores o iguales a 775
+            { min: 1001, max: 1024, ajuste: 230 } // Para alturas mayores o iguales a 775
+        ];
+
+        // Buscar el ajuste correspondiente para la altura dada
+        for (const rango of rangos) {
+            if (altura >= rango.min && altura <= rango.max) {
+                return tamanioDiv + rango.ajuste;
+            }
+        }
+
+        // Si no se encuentra ningún rango, retornar 0 (opcional)
+        return 0;
+    }
+    function ajusteAlturaLogo(altura, tamanioDiv) {
+        // Definir los rangos y ajustes en un array de objetos
+        const rangos = [
+            { min: 300, max: 400, ajuste: 68 },
+            { min: 401, max: 475, ajuste: 66 },
+            { min: 476, max: 575, ajuste: 65 },
+            { min: 576, max: 775, ajuste: 64 }
+        ];
+        for (const rango of rangos) {
+            if (altura >= rango.min && altura <= rango.max) {
+                return rango.ajuste;
+            }
+        }
+        return 0;
+    }
+    function ajusteAlturaLogoTablets(altura, tamanioDiv) {
+        // Definir los rangos y ajustes en un array de objetos
+        const rangos = [
+            { min: 768, max: 1024, ajuste: 62 }
+            // { min: 821, max: 1024, ajuste: 62 }
+            // { min: 876, max: 900, ajuste: 66 },
+            // { min: 901, max: 950, ajuste: 66 },
+            // { min: 951, max: 1000, ajuste: 66 },
+            // { min: 1001, max: 1024, ajuste: 64 }
+        ];
+        for (const rango of rangos) {
+            if (altura >= rango.min && altura <= rango.max) {
+                return rango.ajuste;
+            }
+        }
+        return 0;
+    }
+
+
 
 });
